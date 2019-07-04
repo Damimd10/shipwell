128dbed0ed7381c63bb6844edb8fd9888b209b88
Damian <damianmdst@gmail.com>
2019-07-04 15:19:07 -0300

    Close modal when the request is success

--- src/screens/home/components/EditModal.js
+++ src/screens/home/components/EditModal.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
 import { bool, func, string } from 'prop-types';
 import { Modal, ModalBody, ModalHeader } from 'shards-react';
 
 import StopForm from './StopForm';
+import { STATUS_SUCCESS } from '../../../redux/constants';
 
 const EditModal = ({ address, closeModal, id, name, open }) => {
+  const isSuccess = useSelector(state => state.stops.status === STATUS_SUCCESS);
+
+  useEffect(() => {
+    if (isSuccess) closeModal();
+  }, [isSuccess]);
+
   return (
     <Modal open={open} toggle={closeModal}>
       <ModalHeader>Edit Stop</ModalHeader>
